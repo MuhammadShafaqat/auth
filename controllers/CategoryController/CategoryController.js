6db3a87287d4e7d5e6db3799b42b77f09eced8aa
Muhammad Shafaqat <muhammadshafaqat0344@gmail.com>
2023-09-28 12:49:55 +0500

    update

--- controllers/CategoryController/CategoryController.js
+++ controllers/CategoryController/CategoryController.js
@@ -13,6 +13,54 @@ const getCategories = async (req, res)=>{
          return res.status(500).json({success: false, message: 'server error'})
     }
 }
+//get request for single category
+const getSingleCategory = async (req,res)=>{
+    try {
+        const category = await CategoryModel.findById(req.params.id);
+
+        if (!category) {
+            return res.status(400).json({success:false, message:'category not found against this id'})
+        }
+        return res.status(201).json(category)
+        
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({success: false, message: 'server error'})
+    }
+}
+//put request 
+const updateCategory = async (req, res) => {
+    try {
+        const categoryId = req.params.id;
+        const updatedData = req.body; // Assuming the request body contains the updated category data
+
+        // Use Mongoose to find the category by ID and update it
+        const category = await CategoryModel.findByIdAndUpdate(categoryId, updatedData, { new: true });
+
+        if (!category) {
+            return res.status(404).json({ success: false, message: 'Category not found' });
+        }
+
+        return res.status(200).json({ success: true, message: 'Category successfully updated', updatedCategory: category });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+}
+
+// delete category
+const deleteCategory = async (req,res)=>{
+    try {
+     const category = await CategoryModel.findByIdAndDelete(req.params.id);
+     if (!category) {
+    return res.status(404).json({success: false, message: 'category not found'})
+     }
+     return res.status(200).json({success: true, message: 'category deleted successfully'})
+    } catch (error) {
+     console.error(error)
+     return res.status(500).json({success: false, message: 'Server Error'})
+    }
+ }
 
 // post request
 const createCategory = async (req, res)=>{
@@ -27,5 +75,19 @@ const createCategory = async (req, res)=>{
                  return res.status(500).json({success: false, message: 'server error'})
             }
 }
+//count categories
+const countCategories = async (req, res) => {
+    try {
+      // Use Mongoose to count the documents in the "categories" collection
+      const categoryCount = await CategoryModel.countDocuments();
+  
+      return res.status(200).json({ success: true, count: categoryCount });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+  }
+  
 
-module.exports = {createCategory, getCategories}
+
+module.exports = {createCategory, getCategories, getSingleCategory,deleteCategory,updateCategory,countCategories}
--- routes/category.js
+++ routes/category.js
@@ -1,11 +1,16 @@
 const express = require('express');
 
-const {createCategory,getCategories} = require('../controllers/CategoryController/CategoryController');
+const {createCategory,getCategories, getSingleCategory, deleteCategory, updateCategory, countCategories} = require('../controllers/CategoryController/CategoryController');
 
 const categoryRoutes = express.Router();
 
-categoryRoutes.get('/category', getCategories)
-categoryRoutes.post('/category', createCategory);
+categoryRoutes.get('/api/categories', getCategories);
+categoryRoutes.get('/api/categories/count', countCategories); // Corrected
+categoryRoutes.get('/api/categories/:id', getSingleCategory);
+categoryRoutes.put('/api/categories/:id', updateCategory);
+categoryRoutes.delete('/api/categories/:id', deleteCategory);
+categoryRoutes.post('/api/categories', createCategory);
+
 
 
 module.exports = categoryRoutes;
