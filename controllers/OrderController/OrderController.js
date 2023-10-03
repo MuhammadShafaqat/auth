ed1d55ccc7cf0ccee4891ddc316e54c2e620c15c
Muhammad Shafaqat <muhammadshafaqat0344@gmail.com>
2023-10-03 23:46:22 +0500

    add

--- controllers/OrderController/OrderController.js
+++ controllers/OrderController/OrderController.js
@@ -21,7 +21,20 @@ const getOrder = async (req,res)=>{
 // get request for single order
 const getSingleOrder = async (req,res)=>{
     try {
-        const order = await OrderModel.findById(req.params.id).populate('user','username');
+        // const order = await OrderModel.findById(req.params.id)
+        // .populate('user','username')
+        // .populate('orderItems');
+        //2-In case of populating the product items from an array we use (path, populate)
+        // const order = await OrderModel.findById(req.params.id)
+        // .populate('user','username')
+        // .populate({path: 'orderItems', populate: 'product'});
+        //3- Next we are going to populate the category which is inside the product 
+        const order = await OrderModel.findById(req.params.id)
+        .populate('user','username')
+        .populate({
+            path: 'orderItems', populate: {
+                path: 'product',populate: 'category'}
+            });
         if (!order) {
             return res.status(404).json({success:false,message:'No order found against this id'})
         }
@@ -66,7 +79,62 @@ const createOrder = async (req,res)=>{
         return res.status(500).json({success:false, message:'Internal server error'}) 
     }
 }
+// update request, in order case usually the admin wants just to update the order status
+const updateOrderStatus = async (req,res)=>{
+                    try {
+                        const order = await OrderModel.findByIdAndUpdate(
+                            req.params.id,{
+                        status:req.body.status
+                        }, {new: true})
+                        if (!order) {
+                            return res.status(400).json({success:false, message: 'order do not exist against this id'})
+                        }
+                        return res.status(200).send(order)
+                    } catch (error) {
+                        return res.status(500).json({success:false, message: 'Internal server errorss'})  
+                    }
+}
+// delete category
+//By below code we can only delete the orders not the orderItems
+// const deleteOrder = async (req,res)=>{
+//     try {
+//      const order = await OrderModel.findByIdAndDelete(req.params.id);
+//      if (!order) {
+//     return res.status(404).json({success: false, message: 'Order not found'})
+//      }
+//      return res.status(200).json({success: true, message: 'Order deleted successfully'})
+//     } catch (error) {
+//      console.error(error)
+//      return res.status(500).json({success: false, message: 'Server Error'})
+//     }
+//  }
+// Here is the solution to delete the order as well as orderItems 
+const deleteOrder = async (req,res)=>{
+    try {
+     const order = await OrderModel.findByIdAndRemove(req.params.id).then(async order=>{
+        if (!order) {
+             res.status(404).json({success: false, message: 'Order not found'})
+             return
+             }
+        if (order) {
+            await order.orderItems.map(async orderItem =>{
+              await OrderItemModel.findByIdAndRemove(orderItem);
+            //   if(!orderItem){
+            //     return   res.status(404).json({success: false, message: 'OrderItem not found'})
+                  
+            //   }
+            //   return res.status(200).json({success: false, message: 'OrderItems are also successfully deleted'})
+            })
+        }
+     });
+    
+     return res.status(200).json({success: true, message: 'Order deleted successfully'})
+    } catch (error) {
+     console.error(error)
+     return res.status(500).json({success: false, message: 'Server Error'})
+    }
+ }
 
 
 
-module.exports = {getOrder, getSingleOrder,createOrder}
+module.exports = {getOrder, getSingleOrder,createOrder,updateOrderStatus,deleteOrder}
--- routes/order.js
+++ routes/order.js
@@ -1,11 +1,13 @@
 const express =  require('express');
-const { getOrder, getSingleOrder, createOrder } = require('../controllers/OrderController/OrderController');
+const { getOrder, getSingleOrder, createOrder, updateOrderStatus, deleteOrder } = require('../controllers/OrderController/OrderController');
 
 const orderRoutes = express.Router();
 
 orderRoutes.get('/orders', getOrder);
 orderRoutes.post('/orders', createOrder)
 orderRoutes.get('/orders/:id', getSingleOrder)
+orderRoutes.put('/orders/:id', updateOrderStatus)
+orderRoutes.delete('/orders/:id', deleteOrder)
 
 
 module.exports = orderRoutes
